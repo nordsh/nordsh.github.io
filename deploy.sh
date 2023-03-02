535bb1468d355fe0ef48886eafdbbb5f7ab8bb83
Rosthan Silva <rosthan.silva@nordsh.com>
2023-03-02 10:54:58 -0300

    Subindo Ghub

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env bash
 
-
-#allow passing a commit message
+# Allow passing a commit message
 message=$1
 
+
 if [ "$message" == "" ]; then
-  #use last git commit message for deploying into github pages
+  #Use last git commit message for deploying into github pages
   message=`git log -1 --pretty=%B`
 fi
 
-#generate static site into public & copy files into dist folder
-hugo -d public
-rsync -avh --delete public/* blog/
+git remote add ghub git@github.com:nordsh/nordsh.github.io.git
 
-# push the files into github pages
-cd dist
 git add .
+
 git commit -m "$message"
-git push ghub main #replace "master" with "gh-pages" if you are deploying into a project site
+
+git push ghub main
+
+##################################################################
+# Generate static site into public & copy files into dist folder #
+##################################################################
+git checkout gh-pages 2>/dev/null || git checkout -b gh-pages
+
+hugo -d public
+
+rsync -avh --delete public/* dist
+
+# Push the files into github pages
+cd dist
+
+git remote add ghub git@github.com:nordsh/nordsh.github.io.git
+
+git add .
+
+git commit -m "$message"
+
+git push ghub gh-pages 
+
+# Replace "master" with "gh-pages" if you are deploying into a project site
 
 # go back to source directory
+
 cd ../
